109c68980d428efed75cf1214c176aec95cb035a
YangJie <yangjie6759@163.com>
2024-10-11 17:18:47 +0800

    feat: Improve SPI library compatibility with Arduino-defined SPI interface functions

--- libraries/SPI/src/SPI.cpp
+++ libraries/SPI/src/SPI.cpp
@@ -5,6 +5,10 @@ extern "C" {
 #include "tal_log.h"
 }
 
+#if defined(ARDUINO_TUYA_T2)
+extern "C" void tkl_spi_set_spic_flag(void);
+#endif
+
 using namespace arduino;
 
 SPIClassTuyaOpen::SPIClassTuyaOpen()
@@ -28,7 +32,7 @@ SPIClassTuyaOpen::SPIClassTuyaOpen(TUYA_SPI_NUM_E port)
 
   cfg.role = TUYA_SPI_ROLE_MASTER;
   cfg.mode = TUYA_SPI_MODE0;
-  cfg.type = TUYA_SPI_SOFT_TYPE;
+  cfg.type = TUYA_SPI_AUTO_TYPE;
   cfg.databits = TUYA_SPI_DATA_BIT8;
   cfg.bitorder = TUYA_SPI_ORDER_LSB2MSB;
   cfg.freq_hz = SPI_DEFAULT_CLOCK;
@@ -40,6 +44,10 @@ SPIClassTuyaOpen::SPIClassTuyaOpen(TUYA_SPI_NUM_E port)
 SPIClassTuyaOpen::SPIClassTuyaOpen(TUYA_SPI_NUM_E port, TUYA_SPI_BASE_CFG_T cfg)
 {
   _spiConfigInitAlwaysInline(port, cfg);
+
+#if defined(ARDUINO_TUYA_T2)
+  tkl_spi_set_spic_flag();
+#endif
 }
 
 SPIClassTuyaOpen::~SPIClassTuyaOpen()
@@ -49,16 +57,18 @@ SPIClassTuyaOpen::~SPIClassTuyaOpen()
 
 void SPIClassTuyaOpen::begin()
 {
-  begin(true);
+  // nothing to do
+  return;
 }
 
-void SPIClassTuyaOpen::begin(bool autoCS)
+void SPIClassTuyaOpen::begin(int csPin)
 {
-  _autoCS = autoCS;
+  _csPin = csPin;
 
-  if (_autoCS) {
+  if (-1 != _csPin) {
     pinMode(_csPin, OUTPUT);
-    digitalWrite(_csPin, LOW);
+    digitalWrite(_csPin, HIGH);
+    _cfg.type = TUYA_SPI_SOFT_TYPE;
   }
 }
 
@@ -70,6 +80,11 @@ void SPIClassTuyaOpen::end()
 void SPIClassTuyaOpen::beginTransaction(SPISettings settings)
 {
   OPERATE_RET rt = OPRT_OK;
+
+  if (_isBeginTransaction) {
+    return;
+  }
+  _isBeginTransaction = 1;
 
   _cfg.freq_hz = settings.getClockFreq();
 
@@ -96,17 +111,22 @@ void SPIClassTuyaOpen::beginTransaction(SPISettings settings)
     PR_ERR("spi[%d] init fail, %d", _port, rt);
   }
 
-  if (_autoCS) {
-    digitalWrite(_csPin, HIGH);
+  if (-1 != _csPin) {
+    digitalWrite(_csPin, LOW);
   }
 }
 
 void SPIClassTuyaOpen::endTransaction(void)
 {
+  if (!_isBeginTransaction) {
+    return;
+  }
+  _isBeginTransaction = 0;
+
   tkl_spi_deinit(_port);
 
-  if (_autoCS) {
-    digitalWrite(_csPin, LOW);
+  if (-1 != _csPin) {
+    digitalWrite(_csPin, HIGH);
   }
 }
 
@@ -120,6 +140,7 @@ uint8_t SPIClassTuyaOpen::transfer(uint8_t data)
   if (OPRT_OK == rt) {
     return 0;
   }
+
   return out;
 }
 
@@ -138,7 +159,7 @@ uint16_t SPIClassTuyaOpen::transfer16(uint16_t data)
 
 void SPIClassTuyaOpen::transfer(void *buf, size_t count)
 {
-  tkl_spi_transfer(_port, buf, NULL, count);
+  tkl_spi_send(_port, buf, count);
 }
 
 SPIClassTuyaOpen SPI(TUYA_SPI_NUM_0);
