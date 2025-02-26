6e71bd9dce436599a34da9db7877c911d6174b21
YangJie <shiliu.yang@tuya.com>
2025-02-26 16:17:31 +0800

    feat: Added ESP32 support, including pin definitions and hardware interface implementations

--- tools/packager/packager.py
+++ tools/packager/packager.py
@@ -11,13 +11,29 @@ from .chip_info import chip_info_class
 from .packager_t2 import get_qio_binary_t2
 from .packager_t3_t5 import get_qio_binary_t3_t5
 from .packager_ln882h import get_qio_binary_ln882h
+from .packager_esp32 import get_qio_binary_esp32
 
 class packager_class():
     def __init__(self, chip, compiler_path, tools_path, output_path, sketch_name, sketch_version):
         self.chip_info = chip_info_class(chip, compiler_path, tools_path, output_path, sketch_name, sketch_version)
 
     def elf_to_binary(self):
-        objcopy_tool = os.path.join(self.chip_info.compiler_path, 'bin', 'arm-none-eabi-objcopy')
+        # .elf -> .bin tool
+        objcopy_tool = ''
+        if self.chip_info.chip == 'esp32':
+            if platform.system() == 'Linux':
+                objcopy_tool = os.path.join(self.chip_info.tools_path, 'linux', 'esptool')
+            elif platform.system() == 'Windows':
+                objcopy_tool = os.path.join(self.chip_info.tools_path, 'windows', 'esptool')
+            elif platform.system() == 'Darwin':
+                mac_arch = platform.machine()
+                logging.info(f"MAC machine is: {mac_arch}")
+                objcopy_tool = os.path.join(self.chip_info.tools_path, 'mac', mac_arch, 'esptool')
+            else:
+                logging.error(f"Unknown OS: {platform.system()}")
+                return False
+        else:
+            objcopy_tool = os.path.join(self.chip_info.compiler_path, 'bin', 'arm-none-eabi-objcopy')
         elf_file = os.path.join(self.chip_info.output_path, self.chip_info.sketch_name + '.elf')
         bin_file = os.path.join(self.chip_info.output_path, self.chip_info.sketch_name + '.bin')
 
@@ -32,12 +48,13 @@ class packager_class():
             logging.error(f"{elf_file} file not found")
             return False
 
-        objcopy_command = [
-            objcopy_tool,
-            '-O', 'binary',
-            elf_file,
-            bin_file
-        ]
+        objcopy_command = []
+        if self.chip_info.chip == 'esp32':
+            # TODO: flash_freq and flash_size should be configurable
+            bin_file = os.path.join(self.chip_info.output_path, self.chip_info.sketch_name + '_app.bin')
+            objcopy_command = [objcopy_tool, '--chip', 'esp32', 'elf2image', '--flash_mode', 'dio', '--flash_freq', '40m', '--flash_size', '4MB', '--elf-sha256-offset', '0xb0', '--min-rev-full', '0', '--max-rev-full', '399', '-o', bin_file, elf_file]
+        else:
+            objcopy_command = [objcopy_tool, '-O', 'binary', elf_file, bin_file]
         logging.debug("objcopy_command: " + ' '.join(objcopy_command))
         result = subprocess.run(objcopy_command)
         if result.returncode != 0 or not os.path.exists(bin_file):
@@ -55,6 +72,8 @@ class packager_class():
             return get_qio_binary_t3_t5(self.chip_info)
         elif self.chip_info.chip == 'ln882h':
             return get_qio_binary_ln882h(self.chip_info)
+        elif self.chip_info.chip == 'esp32':
+            return get_qio_binary_esp32(self.chip_info)
         else:
             return False
 
---
+++ variants/esp32/esp32_hw_port.c
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+
+#include "pins_arduino.h"
+
+static TUYA_ADC_BASE_CFG_T sgAdcConfig[TUYA_ADC_NUM_MAX] = {
+  {.ch_list.bits.ch_0=1, .ch_nums=1, .width=12, .mode=TUYA_ADC_CONTINUOUS, .type=TUYA_ADC_INNER_SAMPLE_VOL, .conv_cnt = 1}, // pin14
+  {.ch_list.bits.ch_1=1, .ch_nums=1, .width=12, .mode=TUYA_ADC_CONTINUOUS, .type=TUYA_ADC_INNER_SAMPLE_VOL, .conv_cnt = 1}, // pin15
+  {.ch_list.bits.ch_2=1, .ch_nums=1, .width=12, .mode=TUYA_ADC_CONTINUOUS, .type=TUYA_ADC_INNER_SAMPLE_VOL, .conv_cnt = 1}, // pin20
+  {.ch_list.data=0, .ch_nums=1, .width=12, .mode=TUYA_ADC_CONTINUOUS, .type=TUYA_ADC_INNER_SAMPLE_VOL, .conv_cnt = 1}, // not support
+  {.ch_list.data=0, .ch_nums=1, .width=12, .mode=TUYA_ADC_CONTINUOUS, .type=TUYA_ADC_INNER_SAMPLE_VOL, .conv_cnt = 1}, // not support
+  {.ch_list.data=0, .ch_nums=1, .width=12, .mode=TUYA_ADC_CONTINUOUS, .type=TUYA_ADC_INNER_SAMPLE_VOL, .conv_cnt = 1}, // not support
+  {.ch_list.data=0, .ch_nums=1, .width=12, .mode=TUYA_ADC_CONTINUOUS, .type=TUYA_ADC_INNER_SAMPLE_VOL, .conv_cnt = 1}, // not support
+};
+
+// duty 5000 = 50%
+static TUYA_PWM_BASE_CFG_T sgPwmConfig[TUYA_PWM_NUM_MAX] = {
+  {.polarity=TUYA_PWM_NEGATIVE, .count_mode=TUYA_PWM_CNT_UP, .duty=5000, .cycle=10000, .frequency = 1000}, // pin19
+  {.polarity=TUYA_PWM_NEGATIVE, .count_mode=TUYA_PWM_CNT_UP, .duty=5000, .cycle=10000, .frequency = 1000} // none
+  {.polarity=TUYA_PWM_NEGATIVE, .count_mode=TUYA_PWM_CNT_UP, .duty=5000, .cycle=10000, .frequency = 1000} // none
+  {.polarity=TUYA_PWM_NEGATIVE, .count_mode=TUYA_PWM_CNT_UP, .duty=5000, .cycle=10000, .frequency = 1000} // none
+  {.polarity=TUYA_PWM_NEGATIVE, .count_mode=TUYA_PWM_CNT_UP, .duty=5000, .cycle=10000, .frequency = 1000} // none
+  {.polarity=TUYA_PWM_NEGATIVE, .count_mode=TUYA_PWM_CNT_UP, .duty=5000, .cycle=10000, .frequency = 1000} // none
+};
+
+TUYA_ADC_BASE_CFG_T adcCfgGet(uint8_t pin)
+{
+  switch (pin) {
+    case 14:
+      return sgAdcConfig[0];
+    case 15:
+      return sgAdcConfig[1];
+    case 20:
+      return sgAdcConfig[2];
+    default:
+      return sgAdcConfig[5];
+  }
+}
+
+uint8_t adcPinToChannel(uint8_t pin)
+{
+  switch (pin) {
+    case 14:
+      return 0;
+    case 15:
+      return 1;
+    case 20:
+      return 2;
+    default:
+      return TUYA_ADC_INVALID_VALUE;
+  }
+}
+
+TUYA_ADC_NUM_E adcPinToNum(uint8_t pin)
+{
+  // ADC only one channel
+  return TUYA_ADC_NUM_0;
+}
+
+TUYA_PWM_NUM_E pwmPinToNum(uint8_t pin)
+{
+  switch (pin) {
+    case 19:
+      return TUYA_PWM_NUM_0;
+    default:
+      return TUYA_PWM_NUM_MAX;
+  }
+}
+
+TUYA_PWM_BASE_CFG_T pwmCfgGet(uint8_t pin)
+{
+  switch (pin) {
+    case 19:
+      return sgPwmConfig[0];
+    default:
+      return sgPwmConfig[5];
+  }
+}
